f3f348d8a4577f4d5a62c773f896ecb5de3645c9
Ayush Kaithwas <ayushkaithwas860@gmail.com>
2025-03-27 12:25:00 +0530

    move method added for cars. Also the cars are spawing indefinitively

--- car.py
+++ car.py
@@ -19,3 +19,18 @@ class Car(Turtle):
             car.penup()
             car.goto(random.randint(-290, 290), random.randint(-200, 200))
             self.cars.append(car)  # Store the car
+    
+    def move_car(self):
+
+        for car in self.cars:
+            car.fd(-10)
+    
+    def spawn_cars(self):
+        car = Turtle()
+        car.shape('square')
+        car.color(random.choice(self.COLORS))
+        car.shapesize(stretch_len=4, stretch_wid=1)
+        car.penup()
+        car.goto(random.randint(310 , 320) , random.randint(-200 , 200))
+        self.cars.append(car)
+
--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from turtle import Screen
 from car import Car
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
-car = Car()
+# Create instance of Car class (singular)
+car_manager = Car()  # Changed variable name to avoid conflict
 
-screen.update()
+spawn_condition = 0
+while True:
+    screen.update()
+    time.sleep(0.1)
+    
+    # Access the cars list from the car_manager instance
+    if spawn_condition > 5 :
+        car_manager.spawn_cars()
+        spawn_condition = 0
+    
 
-
-
-
-
-
+    car_manager.move_car()
+    spawn_condition += 1 
 screen.exitonclick()
